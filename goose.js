c664fd5728b34e9447da0a9702e3add43896bce2
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-01 18:08:56 -0400

    goose extension for presentation

--- goose.js
+++ goose.js
@@ -6,40 +6,65 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("honk honk");
   }
 
-  document.addEventListener("mousedown", () => {
-    console.log("mousedown honk working");
-    gooseSound();
-  });
+  let egg = document.getElementById("egg");
+  // document.getElementsByTagName("body").ondblclick = eggDrop;
+  // let doubleClick = document.getElementById("doubleClick");
 
-  document.addEventListener("mousemove", () => {
-    console.log("goose cursor listener working");
-    ballCoords();
-  });
+  // doubleClick.ondblclick = eggDrop;
 
-  var ball = document.getElementById("ball");
-  var moveOnMe = document.getElementById("mouseOnMe");
+  function eggDrop() {
+    // console.log(`${window.innerWidth} windowWidth`);
+    let xEggPosRandom = Math.floor(Math.random() * window.innerWidth);
+    // console.log(`${xEggPosRandom} egg postiion X`);
+    let yEggPosRandom = Math.floor(Math.random() * window.innerHeight);
+    return (egg.style = `
+    left: ${xEggPosRandom}px;
+    top: ${yEggPosRandom}px;
+    `);
+    // egg.style.left =
+    // egg.style.right =
+    // return egg;
 
-  moveOnMe.onmousemove = ballCoords;
+    // console.log(`${egg.style.left} eggstyleTest`);
+  }
 
-  function ballCoords(mouseEvent) {
-    var xpos;
-    var ypos;
+  let goose = document.getElementById("goose");
+  let moveOnMe = document.getElementById("mouseOnMe");
+
+  moveOnMe.onmousemove = gooseCoords;
+
+  function gooseCoords(mouseEvent) {
+    let xpos;
+    let ypos;
 
     if (mouseEvent) {
       xpos = mouseEvent.pageX + "px";
       ypos = mouseEvent.pageY + "px";
     } else {
-      //IE
       xpos = window.event.x + document.body.scrollLeft - 2;
       ypos = window.event.y + document.body.scrollTop - 2;
     }
 
-    ball.style.top = ypos;
-    ball.style.left = xpos;
+    goose.style.top = ypos;
+    goose.style.left = xpos;
   }
 
-  var translateX = ball.style.width;
-  var translateY = ball.style.height;
+  let translateX = goose.style.width;
+  let translateY = goose.style.height;
 
-  ball.style.transform = "translate(" + translateX + ", " + translateY + ")";
+  goose.style.transform = "translate(" + translateX + ", " + translateY + ")";
+
+  document.addEventListener("mousedown", () => {
+    console.log("mousedown honk working");
+    gooseSound();
+  });
+
+  document.addEventListener("dblclick", () => {
+    eggDrop();
+    console.log("egg drop");
+  });
+  // document.addEventListener("mousemove", () => {
+  //   console.log("goose cursor listener working");
+  //   gooseCoords();
+  // });
 });
